d0306a136efcd3d1958cb6787495863252ef885d
Marcelo Miqueles <marcelomiqueles@hotmail.com>
2015-05-07 10:59:36 -0300

    Exportar Datos
    Se puede exportar datos a CSV

--- app/src/main/java/com/example/marcelomiqueles/dbcow/MainActivity.java
+++ app/src/main/java/com/example/marcelomiqueles/dbcow/MainActivity.java
@@ -10,6 +10,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.opencsv.CSVWriter;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class MainActivity extends Activity {
@@ -48,6 +54,8 @@ public class MainActivity extends Activity {
             case R.id.insert:
                 Intent i = new Intent(this, IngresoForm.class);
                 startActivity(i);
+            case R.id.exportar:
+                exportarDB();
         }
         return super.onOptionsItemSelected(item);
     }
@@ -79,6 +87,74 @@ public class MainActivity extends Activity {
                 txtResultado.append(" " + rut + " - " + nom + " " + ape + " - " + mail + "\n");
             } while(c.moveToNext());
         }
+    }
+
+    public static void exportarDB() {
+
+        CSVWriter writer = null;
+
+        // Abrimos la base de datos 'DBUsuarios' en modo escritura
+        PersonasSQLiteHelper usdbh = new PersonasSQLiteHelper(context, "DBPersonas", null, 1);
+
+        db = usdbh.getWritableDatabase();
+
+        //Alternativa 1: método rawQuery()
+        Cursor c = db.rawQuery("SELECT rut, nombre, apellido, mail, telefono FROM Personas", null);
+
+        //Recorremos los resultados para mostrarlos en pantalla
+        try
+        {
+            writer = new CSVWriter(new FileWriter("/sdcard/myfile.csv"), ',');
+            if (c != null) {
+                if (c.moveToFirst()) {
+                    //Recorremos el cursor hasta que no haya más registros
+                    do {
+                        String entrie = c.getString(0) + "#" + c.getString(1) + "#" + c.getString(2) + "#" + c.getString(3) + "#" + c.getString(4);
+                        String[] entries = entrie.split("#");
+                        writer.writeNext(entries);
+                    } while (c.moveToNext());
+                }
+            }
+            writer.close();
+            Toast.makeText(context, "Guardo!!!", Toast.LENGTH_SHORT).show();
+        }
+        catch (IOException e)
+        {
+            //error
+            Toast.makeText(context, "Fallo!!!", Toast.LENGTH_SHORT).show();
+        }
+//        String outFileName = "db.csv";
+//
+//        //Abrimos la base de datos 'DBUsuarios' en modo escritura
+//        PersonasSQLiteHelper usdbh = new PersonasSQLiteHelper(context, "DBPersonas", null, 1);
+//
+//        db = usdbh.getWritableDatabase();
+//
+//        //Alternativa 1: método rawQuery()
+//        Cursor c = db.rawQuery("SELECT rut, nombre, apellido, mail FROM Personas", null);
+//
+//        //Recorremos los resultados para mostrarlos en pantalla
+//        try {
+//            File outFile = new File(outFileName);
+//            FileWriter fileWriter = new FileWriter(outFile);
+//            BufferedWriter out = new BufferedWriter(fileWriter);
+//            if (c != null) {
+//                if (c.moveToFirst()) {
+//                    //Recorremos el cursor hasta que no haya más registros
+//                    do {
+//                        csvValues = c.getString(0) + "#";
+//                        csvValues += c.getString(1) + "#";
+//                        csvValues += c.getString(2) + "#";
+//                        csvValues += c.getString(3);
+//
+//                        out.write(csvValues);
+//                    } while (c.moveToNext());
+//                }
+//            }
+//            out.close();
+//        } catch (IOException e) {
+//            Toast.makeText(context, "Fallo!!!", Toast.LENGTH_SHORT).show();
+//        }
     }
 
     private void createTodo() {
